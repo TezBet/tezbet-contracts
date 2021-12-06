c43462b64f349559124e89f6b3365a55e7172718
Dichiara <edclrcq@gmail.com>
2021-12-06 22:22:49 +0100

    Removed scores from contract storage and replaced risky entry points with regular functions

--- TezBet.py
+++ TezBet.py
@@ -1,15 +1,15 @@
 import smartpy as sp
 
 class Match(sp.Contract):
-    def __init__(self, contract_address):
+    def __init__(self, contract_address, team_a, team_b):
         self.init(
             contract_address = contract_address,
             status = "Not started",
             outcome = "NA",
             total_betted_amount = sp.tez(0),
-            score = sp.map({
-                "team_a": sp.nat(0),
-                "team_b": sp.nat(0)
+            names = sp.map({
+                "team_a": team_a,
+                "team_b": team_b
             }),
             betted_amount = sp.map({
                 "team_a": sp.tez(0),
@@ -25,32 +25,26 @@ class Match(sp.Contract):
             tx = sp.map(l = {}, tkey = sp.TAddress, tvalue = sp.TRecord(amount = sp.TMutez, choice = sp.TString))
         )
 
-    # This entry point is for development purposes, we don't need to store the actual scores in our contract
-    # since we can call them directly from the infer_outcome function
-    @sp.entry_point
+    # This function will be called by the contract after each oracle call
     def update_score(self, team, score):
         sp.verify_equal(self.data.status, "Playing", "Error: match must be playing to update the score")
         sp.verify_equal(sp.sender, self.data.contract_address, "Error: you are not allowed to update the score")
         self.data.score[team] = score
 
-    @sp.entry_point
+    # This function will be called by the contract after each oracle call
     def update_status(self, new_match_status):
         sp.verify_equal(sp.sender, self.data.contract_address, "Error: you are not allowed to update the match status")
         sp.if new_match_status in ("Playing", "Not started", "Suspended", "Ended"):
             self.data.status = new_match_status
-
 
     @sp.entry_point
     def place_bet(self, choice):
         # Before placing a bet, we make sure the match has not started
         sp.verify(self.data.status == "Not started", message = "Error: you cannot place a bet anymore")
         sp.verify(sp.amount > sp.mutez(0), message = "Error: your bet cannot be null")
-        sp.if self.data.tx.contains(sp.sender):
-            sp.failwith("Error: you have already placed a bet, please remove it before placing a new one")
-        sp.else: 
-            self.data.betted_amount[choice] += sp.amount
-            self.data.tx[sp.sender] = sp.record(amount = sp.amount, choice = choice)
-            self.total_betted_amount = self.update_rating()
+        self.data.betted_amount[choice] += sp.amount
+        self.data.tx[sp.sender] = sp.record(amount = sp.amount, choice = choice)
+        self.total_betted_amount = self.update_rating()
 
     @sp.entry_point
     def remove_bet(self):
@@ -76,22 +70,15 @@ class Match(sp.Contract):
                 self.data.rating[key] = rating.open_some()
             sp.else:
                 self.data.rating[key] = sp.pair(sp.nat(1), sp.tez(0))
-
-    # This entry point is a sketch for the upcoming oracle contract -- here it only serves scenario purposes
-    @sp.entry_point
-    def infer_outcome(self):
-        sp.verify_equal(sp.sender, self.data.contract_address, "Error: you are not allowed to update the match status")
-        sp.verify(self.data.status == "Ended", "Error: you cannot call this function unless match has ended")
-        sp.if self.data.score["team_a"] > self.data.score["team_b"]:
-            self.data.outcome = "team_a"
-        sp.if self.data.score["team_a"] < self.data.score["team_b"]:
-            self.data.outcome = "team_b"
-        sp.if self.data.score["team_a"] == self.data.score["team_b"]:
-            self.data.outcome = "tie"
     
     # We let the users retrieve their gains themselves so they pay for this smart contract execution
     @sp.entry_point
     def redeem_tez(self):
+
+        # The two following lines are here for testing purposes while the oracle calls have not be set.
+        self.data.status = "Ended"
+        self.data.outcome = "team_a"
+
         sp.verify_equal(self.data.status, "Ended", "Error: you cannot redeem your gains before the match has ended")
         sp.verify_equal(self.data.tx[sp.sender].choice, self.data.outcome, "Error: you have lost the bet")
         offset = 1000000
@@ -107,27 +94,13 @@ def test():
     # A set of functions cannot be performed by anyone but the contract itself, updating the score for instance.
     contract = sp.test_account("Contract")
 
-    test_match = Match(contract.address)
+    test_match = Match(contract.address, "France", "Italie")
     scenario += test_match
-
-    # Updating the score tests
-    scenario += test_match.update_status("Playing").run(sender = contract.address)
-    scenario.verify(test_match.data.status == "Playing")
-    scenario += test_match.update_score(team = "team_a", score = sp.nat(2)).run(sender = contract.address)
-    scenario += test_match.update_score(team = "team_b", score = sp.nat(1)).run(sender = contract.address)
-
-    # Match status tests
-    scenario += test_match.update_status("Suspended").run(sender = contract.address)
-    scenario.verify(test_match.data.status == "Suspended")
-    scenario += test_match.update_status("Not started").run(sender = contract.address)
-    scenario.verify(test_match.data.status == "Not started")
 
     bob = sp.test_account("Bob")
     alice = sp.test_account("Alice")
     p_a = sp.test_account("Pierre-Antoine")
     garfield = sp.test_account("Garfield")
-    scenario += test_match.update_status("Suspended").run(valid = False, sender = bob.address)
-    scenario.verify(test_match.data.status != "Suspended")
     
     # Placing and removing bets tests
     scenario += test_match.place_bet("team_a").run(sender = bob.address, amount = sp.mutez(7500))
@@ -138,11 +111,5 @@ def test():
     scenario += test_match.place_bet("team_b").run(sender = bob.address, amount = sp.mutez(23))
     scenario += test_match.place_bet("tie").run(sender = garfield.address, amount = sp.mutez(784))
 
-    # Defining outcome tests
-    scenario += test_match.update_status("Ended").run(sender = contract.address)
-    scenario += test_match.infer_outcome().run(sender = contract.address)
-    scenario.verify(test_match.data.outcome == "team_a")
-
     # Sending XTZ tests
     scenario += test_match.redeem_tez().run(sender = alice.address)
-    scenario += test_match.update_status("Started").run(sender = contract.address)
